686b33eabf6cd9fb388b2770dad8f77b7f33637a
R M Shahidul Islam Shahed <shahed.mbstu@gmail.com>
2017-08-22 10:58:20 +0600

    + Project added

---
+++ Sln.ProgrammingProblems/ProblemSet/GeneralProblem/PalindromeNumber.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace ProblemSet.GeneralProblem
+{
+    static class PalindromeNumber
+    {
+        public static void IMPPalindrome()
+        {
+            var input = Console.ReadLine();
+            Console.WriteLine(IsPalindromeUsingLINQ(input.ToString()));
+        }
+        public static bool IsPalindromeUsingLINQ(string word)
+        {
+            return word.SequenceEqual(word.Reverse());
+        }
+
+
+
+        public static void PalindromeRawLogic()
+        {
+            while (true)
+            {
+                int number, tempNumber, remainder, reverse = 0;
+                Console.Write("Enter an integer: ");
+                number = int.Parse(Console.ReadLine());
+
+                tempNumber = number;
+
+                //Main Logic
+                while (number > 0)
+                {
+                    remainder = number % 10;
+                    reverse = reverse * 10 + remainder;
+                    number = number / 10;
+                }
+
+                Console.WriteLine("Given number is = {0}", tempNumber);
+                Console.WriteLine("Its reverse is  = {0}", reverse);
+
+                if (tempNumber == reverse)
+                    Console.WriteLine("Given number is a palindrome.\n");
+                else
+                    Console.WriteLine("Given number is not a palindrome.\n");
+            }
+
+        }
+
+    }
+}
---
+++ Sln.ProgrammingProblems/ProblemSet/Program.cs
@@ -0,0 +1,16 @@
+﻿using ProblemSet.GeneralProblem;
+using System;
+
+namespace ProblemSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            PalindromeNumber.PalindromeRawLogic();
+            Console.ReadLine();
+        }
+    }
+}
